2aa2806fa5ccb9a666adc504c9a4b78aa06e318e
siparka <106434863+siparka@users.noreply.github.com>
2026-07-03 02:30:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-print("=" * 40)
-print("VK_TOKEN =", os.getenv("VK_TOKEN"))
-print("CHAT_ID =", os.getenv("CHAT_ID"))
-print("=" * 40)
-
-VK_TOKEN = os.getenv("VK_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+VK_TOKEN = os.environ.get("VK_TOKEN")
+CHAT_ID = int(os.environ.get("CHAT_ID", "2000000001"))
