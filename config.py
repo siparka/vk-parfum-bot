b42f8c3812766e799556a30208c2da9e536596b8
siparka <106434863+siparka@users.noreply.github.com>
2026-07-03 02:12:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,13 @@ import os
 
 load_dotenv()
 
+print("VK_TOKEN =", os.getenv("VK_TOKEN"))
+print("CHAT_ID =", os.getenv("CHAT_ID"))
+
 VK_TOKEN = os.getenv("VK_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+
+chat = os.getenv("CHAT_ID")
+if chat is None:
+    raise ValueError("CHAT_ID не найден в переменных окружения!")
+
+CHAT_ID = int(chat)
